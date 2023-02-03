4f8194e34a1da314f97095a320cea18508110ffd
John Chacpi <johnchacpi479@gmail.com>
2023-02-03 13:12:03 -0500

    created dynamic routes for posts

--- app/posts/ListOfPosts.jsx
+++ app/posts/ListOfPosts.jsx
@@ -1,16 +1,24 @@
+import { LikeButton } from "./LikeButton";
+import Link from "next/link"
 
 const fetchPosts = () => {
-    return fetch("https://jsonplaceholder.typicode.com/posts")
-        .then(res => res.json())
+    console.log("FETCHING")
+    return fetch("https://jsonplaceholder.typicode.com/posts", { next: {
+        revalidate: 10
+    } })
+        .then(res => res.json());
 }
 
 export async function ListOfPosts({ params }) {
     
-    const posts = await fetchPosts()
+    const posts = await fetchPosts();
     return posts.slice(0, 5).map(post => (
         <article style={{color: "#09f"}} key={post.id}>
-            <h2>{post.title}</h2>
-            <p>{post.body}</p>
+            <Link href={`/posts/${post.id}`}>
+                <h2>{post.title}</h2>
+                <p>{post.body}</p>
+                <LikeButton id={post.id}/>
+            </Link>
         </article>
     ))
 }
