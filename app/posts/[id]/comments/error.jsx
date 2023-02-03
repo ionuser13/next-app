f745809ceff22352c6a2d3c38b3f5fb002fc07fb
John Chacpi <johnchacpi479@gmail.com>
2023-02-03 18:55:29 -0500

    created error pages

---
+++ app/posts/[id]/comments/error.jsx
@@ -0,0 +1,9 @@
+"use client"
+
+export default function Error () {
+    return (
+        <p style={{background: "red", color: "white"}}>
+            Error when loading the comments
+        </p>
+    )
+}
